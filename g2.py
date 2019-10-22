7a4bd1ea0f15e97b9a519fad396608f965fc65cf
m1-1 <56885539+m1-1@users.noreply.github.com>
2019-10-22 21:11:10 +0300

    Create g2.py

---
+++ g2.py
@@ -0,0 +1,51 @@
+import os
+import json
+import subprocess
+
+import fire
+
+
+def generate(title, author, date, domain, config="lm/configs/mega.json", checkpoint="models/mega/model.ckpt"):
+    params = {
+        "url": "",
+        "url_used": "",
+        "title": f"{title}",
+        "text": "",
+        "summary": "We’ve got budget, retirement account, credit, information security and insurance advice for your independent adult, college student, gap-year taker or future soldier.",
+        "authors": [f"{author}"],
+        "publish_date": f"{date}",
+        "domain": f"{domain}",
+        "warc_date": "",
+        "status": "",
+        "split": "gen",
+        "inst_index": 1,
+    }
+
+    print("\nParameters:\n")
+    print(f"Title: {title}")
+    print(f"Author: {author}")
+    print(f"Date: {date}")
+    print(f"Domain: {domain}")
+    print(f"Checkpoint: {checkpoint}")
+
+    with open("params.jsonl", "w") as f:
+        json.dump(params, f)
+
+    subprocess.run(
+        f"PYTHONPATH=$(pwd) python sample/contextual_generate.py -model_config_fn {config} -model_ckpt {checkpoint} -metadata_fn params.jsonl -out_fn generated.jsonl",
+        stdout=open(os.devnull, "w"),
+        stderr=subprocess.STDOUT,
+        shell=True,
+    )
+
+    with open("generated.jsonl") as f:
+        data = json.load(f)
+        print("\nGenerated text:\n")
+        print(data["gens_article"][0])
+
+    os.system("rm params.jsonl")
+    os.system("rm generated.jsonl")
+
+
+if __name__ == "__main__":
+    fire.Fire(generate)
